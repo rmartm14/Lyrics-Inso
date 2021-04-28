7768a102d27dcca69c4a786fb3ff5754a5284642
rmartm14 <rmartm14@estudiantes.unileon.es>
2021-04-28 16:47:19 +0200

    Commit añadiendo instruments methods

--- Lyrics-Inso/src/main/java/es/unileon/inso2/lyrics/modelo/Instruments.java
+++ Lyrics-Inso/src/main/java/es/unileon/inso2/lyrics/modelo/Instruments.java
@@ -7,6 +7,7 @@ package es.unileon.inso2.lyrics.modelo;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -34,5 +35,77 @@ public class Instruments implements Serializable{
     @JoinColumn(name="style")
     @ManyToOne
     private Styles style;
+
+    public int getInstrument_id() {
+        return instrument_id;
+    }
+
+    public void setInstrument_id(int instrument_id) {
+        this.instrument_id = instrument_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public Styles getStyle() {
+        return style;
+    }
+
+    public void setStyle(Styles style) {
+        this.style = style;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 79 * hash + this.instrument_id;
+        hash = 79 * hash + Objects.hashCode(this.name);
+        hash = 79 * hash + Float.floatToIntBits(this.price);
+        hash = 79 * hash + Objects.hashCode(this.style);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Instruments other = (Instruments) obj;
+        if (this.instrument_id != other.instrument_id) {
+            return false;
+        }
+        if (Float.floatToIntBits(this.price) != Float.floatToIntBits(other.price)) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.style, other.style)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+            
 
 }
