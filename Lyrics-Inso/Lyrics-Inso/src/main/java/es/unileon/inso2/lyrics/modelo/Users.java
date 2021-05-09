862f76c963e87758e46342d45d2d5c45ff77b164
rmartm14 <rmartm14@estudiantes.unileon.es>
2021-05-09 12:32:16 +0200

    Unify Styles

---
+++ Lyrics-Inso/Lyrics-Inso/src/main/java/es/unileon/inso2/lyrics/modelo/Users.java
@@ -0,0 +1,134 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.unileon.inso2.lyrics.modelo;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Sam
+ */
+@Entity
+@Table(name="users")
+public class Users implements Serializable{
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private int user_id;
+    @Column (name="name")
+    private String name;
+    @Column (name="password")
+    private String password;
+    @Column (name="grade")
+    private float grade;
+    @Column (name="fecha_nacimiento")
+    private Date fecha_nacimiento;
+    @Column (name="role")
+    private boolean role;
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 97 * hash + this.user_id;
+        hash = 97 * hash + Objects.hashCode(this.name);
+        hash = 97 * hash + Objects.hashCode(this.password);
+        hash = 97 * hash + Float.floatToIntBits(this.grade);
+        hash = 97 * hash + Objects.hashCode(this.fecha_nacimiento);
+        hash = 97 * hash + (this.role ? 1 : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Users other = (Users) obj;
+        if (this.user_id != other.user_id) {
+            return false;
+        }
+        if (Float.floatToIntBits(this.grade) != Float.floatToIntBits(other.grade)) {
+            return false;
+        }
+        if (this.role != other.role) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.password, other.password)) {
+            return false;
+        }
+        if (!Objects.equals(this.fecha_nacimiento, other.fecha_nacimiento)) {
+            return false;
+        }
+        return true;
+    }
+
+    
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public float getGrade() {
+        return grade;
+    }
+
+    public void setGrade(float grade) {
+        this.grade = grade;
+    }
+
+    public Date getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+
+    public void setFecha_nacimiento(Date fecha_nacimiento) {
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+
+    public boolean isRole() {
+        return role;
+    }
+
+    public void setRole(boolean role) {
+        this.role = role;
+    }
+            
+}
