7d6975890a0102b3dd91c47dc2f244b92abb747e
slopel02 <Sam@DESKTOP-ESJ1VES.mshome.net>
2021-05-14 21:32:04 +0200

    Estructura vistas

--- Lyrics-Inso/target/generated-sources/annotations/es/unileon/inso2/lyrics/modelo/Songs_.java
+++ Lyrics-Inso/target/generated-sources/annotations/es/unileon/inso2/lyrics/modelo/Songs_.java
@@ -9,7 +9,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-05-14T19:53:45")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-05-14T21:30:35")
 @StaticMetamodel(Songs.class)
 public class Songs_ { 
 
