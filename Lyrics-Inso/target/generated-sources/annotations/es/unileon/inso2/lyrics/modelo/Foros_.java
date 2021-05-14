03192aacd28f89ff622f9e0c6c9194b2ddb28a0c
slopel02 <Sam@DESKTOP-ESJ1VES.mshome.net>
2021-05-14 20:05:28 +0200

    Uso de template con pagina inicial

--- Lyrics-Inso/target/generated-sources/annotations/es/unileon/inso2/lyrics/modelo/Foros_.java
+++ Lyrics-Inso/target/generated-sources/annotations/es/unileon/inso2/lyrics/modelo/Foros_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-05-13T19:39:05")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-05-14T19:53:45")
 @StaticMetamodel(Foros.class)
 public class Foros_ { 
 
