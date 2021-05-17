20c64dfb3eee301bcbfebbbbef5642a4a56fbbe4
amunis01 <amunis01@estudiantes.unileon.es>
2021-05-17 16:21:54 +0200

    Rama para el formulario de creacion y modificacion de grupo

--- Lyrics-Inso/target/generated-sources/annotations/es/unileon/inso2/lyrics/modelo/Instruments_.java
+++ Lyrics-Inso/target/generated-sources/annotations/es/unileon/inso2/lyrics/modelo/Instruments_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-05-14T21:30:35")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-05-17T15:14:14")
 @StaticMetamodel(Instruments.class)
 public class Instruments_ { 
 
@@ -15,6 +15,7 @@ public class Instruments_ {
     public static volatile SingularAttribute<Instruments, Float> price;
     public static volatile ListAttribute<Instruments, Songs> songs;
     public static volatile SingularAttribute<Instruments, String> name;
+    public static volatile SingularAttribute<Instruments, String> instrumentstyle;
     public static volatile SingularAttribute<Instruments, Integer> instrument_id;
 
 }
--- Lyrics-Inso/target/generated-sources/annotations/es/unileon/inso2/lyrics/modelo/Songs_.java
+++ Lyrics-Inso/target/generated-sources/annotations/es/unileon/inso2/lyrics/modelo/Songs_.java
@@ -9,19 +9,19 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-05-14T21:30:35")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-05-17T15:14:14")
 @StaticMetamodel(Songs.class)
 public class Songs_ { 
 
     public static volatile SingularAttribute<Songs, Integer> song_id;
     public static volatile SingularAttribute<Songs, Boolean> original;
+    public static volatile SingularAttribute<Songs, Users> user_id;
+    public static volatile SingularAttribute<Songs, Group> group_id;
     public static volatile SingularAttribute<Songs, Float> grade;
     public static volatile SingularAttribute<Songs, String> name;
     public static volatile SingularAttribute<Songs, Styles> style;
-    public static volatile SingularAttribute<Songs, Users> user;
     public static volatile SingularAttribute<Songs, String> lyrics;
     public static volatile SingularAttribute<Songs, Integer> visit_counter;
-    public static volatile SingularAttribute<Songs, Group> group;
     public static volatile ListAttribute<Songs, Instruments> instrumentos;
 
 }
