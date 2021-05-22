534ee4a0add9e52b061545d36d413f8d7745ed0e
rmartm14 <rmartm14@estudiantes.unileon.es>
2021-05-22 12:32:13 +0200

    Commit con editar user

--- Lyrics-Inso/src/main/java/es/unileon/inso2/lyrics/controlador/UserOptionsController.java
+++ Lyrics-Inso/src/main/java/es/unileon/inso2/lyrics/controlador/UserOptionsController.java
@@ -24,11 +24,15 @@ import javax.inject.Named;
 public class UserOptionsController implements Serializable{
     @EJB
     private UsersFacadeLocal userEJB;
+    
     private Users user;
+    
+    private String oldVal, newVal, newValConfirm;
     
     @PostConstruct
     public void init() {
         user = (Users) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("usuario");
+        
     }
     
     public String showEditar() {
@@ -41,6 +45,45 @@ public class UserOptionsController implements Serializable{
         FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("usuario", user);
         FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Registrar canción", "Usuario editado con exito");
         FacesContext.getCurrentInstance().addMessage(null, message);  
+    }
+    
+    public void editarPass() {
+        int flag = this.comprobarPass();
+        
+        //Completamente comprobado
+        if (flag == 0){
+            this.user.setPassword(newVal);
+            this.editar();
+            oldVal = newVal= newValConfirm = "";
+            
+        }
+        //Falla la pass actual-> flag 1
+        if(flag == 1){
+            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error en contraseña", "La contraseña actual no coincide");
+            FacesContext.getCurrentInstance().addMessage(null, message);  
+        }
+        //Falla la pass comprobada -> flag 2
+        if(flag == 2){
+            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error en contraseña", "Las nuevas contraseñas no coinciden.");
+            FacesContext.getCurrentInstance().addMessage(null, message);  
+        }
+    }
+    
+    public int comprobarPass(){
+        boolean actual = this.oldVal.equals(this.user.getPassword());
+        boolean confirm = this.newVal.equals(this.newValConfirm);
+        int returnValue = 0;
+        if(actual == true  && confirm == true){
+            returnValue = 0;
+        }
+        else if(actual == false){
+            returnValue = 1;
+        }
+        else{
+            returnValue = 2;
+        }
+        
+       return returnValue;
     }
 
     public UsersFacadeLocal getUserEJB() {
@@ -58,5 +101,30 @@ public class UserOptionsController implements Serializable{
     public void setUser(Users user) {
         this.user = user;
     }
+
+    public String getOldVal() {
+        return oldVal;
+    }
+
+    public void setOldVal(String oldVal) {
+        this.oldVal = oldVal;
+    }
+
+    public String getNewVal() {
+        return newVal;
+    }
+
+    public void setNewVal(String newVal) {
+        this.newVal = newVal;
+    }
+
+    public String getNewValConfirm() {
+        return newValConfirm;
+    }
+
+    public void setNewValConfirm(String newValConfirm) {
+        this.newValConfirm = newValConfirm;
+    }
+    
     
 }
